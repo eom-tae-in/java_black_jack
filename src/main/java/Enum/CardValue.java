86d616df6f65741e5b840ae1ded3519b2fe52756
eom-tae-in <eti0728@daum.net>
2023-06-02 04:05:00 +0900

    [feat] 카드 값을 나타내는 enum 클래스 생성

---
+++ src/main/java/Enum/CardValue.java
@@ -0,0 +1,18 @@
+package Enum;
+
+public enum CardValue {
+    ACE,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}

069b1fc883924132a87c2a855952791343ba01f4
eom-tae-in <eti0728@daum.net>
2023-06-02 05:27:44 +0900

    [feat] Value 인스턴스를 생성할 때 value 값이 전체 카드 덱에 존재하는 값이 맞는지 검증하는 로직 추가

--- src/main/java/domain/Value.java
+++ src/main/java/domain/Value.java
@@ -4,7 +4,7 @@ public class Value {
     private final String value;
 
     public Value(String value) {
-        this.value = value;
+        this.value = CardValue.validate(value);
     }
 
     public String getValue() {

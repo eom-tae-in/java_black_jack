fdb29f745fc5c535d697b093b849083bdda7b013
eom-tae-in <eti0728@daum.net>
2023-06-02 04:43:42 +0900

    [feat] Card 클래스에 shape, value를 인자로 가지는 생성자 추가

--- src/main/java/entity/Card.java
+++ src/main/java/entity/Card.java
@@ -3,4 +3,9 @@ package entity;
 public class Card {
     String shape;
     String value;
+
+    public Card(String shape, String value) {
+        this.shape = shape;
+        this.value = value;
+    }
 }

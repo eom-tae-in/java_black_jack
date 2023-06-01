d9ff43fc7637978f8461b4062e38e112842ad398
eom-tae-in <eti0728@daum.net>
2023-06-02 05:25:43 +0900

    [feat] value 값이 카드 전체 덱에 존재하는 값인지 검증하는 메서드 추가

--- src/main/java/domain/CardValue.java
+++ src/main/java/domain/CardValue.java
@@ -1,18 +1,31 @@
 package domain;
 
+import java.util.Arrays;
+
 public enum CardValue {
-    ACE,
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING
+    ACE(new Value("ace")),
+    ONE(new Value("one")),
+    TWO(new Value("two")),
+    THREE(new Value("three")),
+    FOUR(new Value("four")),
+    FIVE(new Value("five")),
+    SIX(new Value("six")),
+    SEVEN(new Value("seven")),
+    EIGHT(new Value("eight")),
+    NINE(new Value("nine")),
+    TEN(new Value("ten")),
+    JACK(new Value("jack")),
+    QUEEN(new Value("queen")),
+    KING(new Value("king"));
+
+    private final Value value;
+
+    CardValue(Value value) {
+        this.value = value;
+    }
+
+    static String validate(String cardValue) {
+        return Arrays.stream(values()).filter(v -> v.value.getValue().equals(cardValue))
+                .findAny().orElseThrow(IllegalArgumentException::new).value.getValue();
+    }
 }

d0ea913e6b0409e1c505dab18ba344833e9178af
eom-tae-in <eti0728@daum.net>
2023-06-02 04:39:22 +0900

    [feat] CardShape enum 클래스에 Shape 인스턴스를 생성하는 로직 추가

--- src/main/java/domain/CardShape.java
+++ src/main/java/domain/CardShape.java
@@ -1,8 +1,18 @@
 package domain;
 
 public enum CardShape {
-    SPADE,
-    HEART,
-    DIAMOND,
-    CLOVER
+    SPADE(new Shape("spade")),
+    HEART(new Shape("heart")),
+    DIAMOND(new Shape("diamond")),
+    CLOVER(new Shape("clover"));
+
+    private final Shape shape;
+
+    CardShape(Shape shape) {
+        this.shape = shape;
+    }
+
+    static Shape getShape(CardShape cardShape) {
+        return cardShape.shape;
+    }
 }

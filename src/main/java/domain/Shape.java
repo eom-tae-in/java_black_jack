794853cffbc4cd228844674c889c5b341b5b200f
eom-tae-in <eti0728@daum.net>
2023-06-02 04:31:09 +0900

    [feat] 모양 데이터를 감싸주는 Shape 클래스 생성

---
+++ src/main/java/domain/Shape.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Shape {
+    private final String shape;
+
+    public Shape(String shape) {
+        this.shape = shape;
+    }
+
+    public String getShape() {
+        return shape;
+    }
+}

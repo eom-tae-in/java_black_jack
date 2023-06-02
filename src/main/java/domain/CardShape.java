46d2612b7652a836b86cfd96a2d998cc3fa72f5f
eom-tae-in <eti0728@daum.net>
2023-06-02 15:49:13 +0900

    [feat] 문양이 카드 문양이 맞는지 검증하는 메서드 추가

--- src/main/java/domain/CardShape.java
+++ src/main/java/domain/CardShape.java
@@ -1,5 +1,9 @@
 package domain;
 
+import exception.NotExistShapeException;
+
+import java.util.Arrays;
+
 public enum CardShape {
     SPADE(new Shape("스페이드")),
     HEART(new Shape("하트")),
@@ -10,5 +14,10 @@ public enum CardShape {
 
     CardShape(Shape shape) {
         this.shape = shape;
+    }
+
+    public static Shape validate(Shape cardShape) {
+        return Arrays.stream(values()).filter(s -> s.shape.getShape().equals(cardShape.getShape()))
+                .findAny().orElseThrow(NotExistShapeException::new).shape;
     }
 }

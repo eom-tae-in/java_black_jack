43ed6a3f4f6dd70a5c40181c11789a4fbb954eab
eom-tae-in <eti0728@daum.net>
2023-06-02 15:24:53 +0900

    [feat] CardShape에 Shape 필드 추가

--- src/main/java/domain/CardShape.java
+++ src/main/java/domain/CardShape.java
@@ -1,8 +1,14 @@
 package domain;
 
 public enum CardShape {
-    SPADE,
-    HEART,
-    DIAMOND,
-    CLOVER;
+    SPADE(new Shape("스페이드")),
+    HEART(new Shape("하트")),
+    DIAMOND(new Shape("다이아몬드")),
+    CLOVER(new Shape("클로버"));
+
+    private final Shape shape;
+
+    CardShape(Shape shape) {
+        this.shape = shape;
+    }
 }

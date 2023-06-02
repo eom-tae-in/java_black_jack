02442e3302c18b85a2be57f86a1ef87d18015511
eom-tae-in <eti0728@daum.net>
2023-06-02 15:30:24 +0900

    [feat] CardValue에 Value 필드 추가

--- src/main/java/domain/CardValue.java
+++ src/main/java/domain/CardValue.java
@@ -2,17 +2,23 @@ package domain;
 
 public enum CardValue {
 
-    ACE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING
+    ACE(new Value("A", 1, 11)),
+    TWO(new Value("2", 2)),
+    THREE(new Value("3", 3)),
+    FOUR(new Value("4", 4)),
+    FIVE(new Value("5", 5)),
+    SIX(new Value("6", 6)),
+    SEVEN(new Value("7", 7)),
+    EIGHT(new Value("8", 8)),
+    NINE(new Value("9", 9)),
+    TEN(new Value("10", 10)),
+    JACK(new Value("J", 10)),
+    QUEEN(new Value("Q", 10)),
+    KING(new Value("K", 10));
+
+    private final Value value;
+
+    CardValue(Value value) {
+        this.value = value;
+    }
 }

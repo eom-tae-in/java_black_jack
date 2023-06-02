c65ec658a2606be260e8b4c688487eb4f4efd725
eom-tae-in <eti0728@daum.net>
2023-06-02 15:20:22 +0900

    [feat] CardValue enum 추상 클래스 생성

---
+++ src/main/java/domain/CardValue.java
@@ -0,0 +1,18 @@
+package domain;
+
+public enum CardValue {
+
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}

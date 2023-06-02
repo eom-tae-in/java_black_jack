fdffb663dedbc128e8be9d6155687e7f7f31d0ff
eom-tae-in <eti0728@daum.net>
2023-06-02 15:27:22 +0900

    [feat] Value 클래스에 카드의 숫자를 담는 필드 추가

--- src/main/java/domain/Value.java
+++ src/main/java/domain/Value.java
@@ -3,9 +3,11 @@ package domain;
 public class Value {
 
     private final String value;
+    private final int[] numbers;
 
-    public Value(String value) {
+    public Value(String value, int ... numbers) {
         this.value = value;
+        this.numbers = numbers;
     }
 
     public String getValue() {

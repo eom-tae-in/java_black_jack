021563776ef696953246600b8f5fe1f702d4bd75
eom-tae-in <eti0728@daum.net>
2023-06-02 15:36:10 +0900

    [feat] Card 클래스에 shape, value 필드 추가

--- src/main/java/entity/Card.java
+++ src/main/java/entity/Card.java
@@ -1,4 +1,23 @@
 package entity;
 
+import domain.Shape;
+import domain.Value;
+
 public class Card {
+
+    private final Shape shape;
+    private final Value value;
+
+    public Card(Shape shape, Value value) {
+        this.shape = shape;
+        this.value = value;
+    }
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public Value getValue() {
+        return value;
+    }
 }

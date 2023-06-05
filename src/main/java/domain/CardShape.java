36b1e81b7f92d73aa217952bce71bea9a0c53d1c
eom-tae-in <eti0728@daum.net>
2023-06-06 08:16:40 +0900

    [feat] 카드 문양을 정의하는 enum 클래스 생성

---
+++ src/main/java/domain/CardShape.java
@@ -0,0 +1,18 @@
+package domain;
+
+public enum CardShape {
+    SPADE(new Shape("스페이드")),
+    HEART(new Shape("하트")),
+    DIAMOND(new Shape("다이아몬드")),
+    CLOVER(new Shape("클로버"));
+
+    private final Shape shape;
+
+    CardShape(Shape shape) {
+        this.shape = shape;
+    }
+
+    public Shape getShape() {
+        return shape;
+    }
+}

324df3887fb0b8075cce5ff6189a4a58a6e0572c
eom-tae-in <eti0728@daum.net>
2023-06-06 08:17:02 +0900

    [feat] 카드 value를 정의하는 enum 클래스 생성

---
+++ src/main/java/domain/CardValue.java
@@ -0,0 +1,28 @@
+package domain;
+
+public enum CardValue {
+
+    ACE(new Value(1)),
+    TWO(new Value(2)),
+    THREE(new Value(3)),
+    FOUR(new Value(4)),
+    FIVE(new Value(5)),
+    SIX(new Value(6)),
+    SEVEN(new Value(7)),
+    EIGHT(new Value(8)),
+    NINE(new Value(9)),
+    TEN(new Value(10)),
+    JACK(new Value(10)),
+    QUEEN(new Value(10)),
+    KING(new Value(10));
+
+    private final Value value;
+
+    CardValue(Value value) {
+        this.value = value;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+}

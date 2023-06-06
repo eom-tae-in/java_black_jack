36203312b642944b72077a23deff73d252dd67de
eom-tae-in <eti0728@daum.net>
2023-06-07 07:19:20 +0900

    [feat] 플레이어에게 카드를 더 받을 것인지 묻는 메서드 생성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,11 @@
 package view;
 
+import domain.Player;
+import domain.Players;
+
 public class OutputView {
+
+    public void AskMore(Player player) {
+        System.out.println(player.getName() + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
+    }
 }

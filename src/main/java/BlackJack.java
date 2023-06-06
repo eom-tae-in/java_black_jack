be4b064e6adf21c54e50cecef76d98118f8c581b
eom-tae-in <eti0728@daum.net>
2023-06-07 07:04:40 +0900

    [feat] BalckJack 클래스 생성

---
+++ src/main/java/BlackJack.java
@@ -0,0 +1,10 @@
+import controller.Controller;
+
+public class BlackJack {
+
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        controller.start();
+    }
+}
+

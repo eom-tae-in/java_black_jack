e15f613d135f35d5551d7b197a11a6166549f861
eom-tae-in <eti0728@daum.net>
2023-06-07 08:21:04 +0900

    [feat] 플레이어와 딜러의 승패 여부를 나타내기 위한 Result 클래스 추가

---
+++ src/main/java/domain/Result.java
@@ -0,0 +1,30 @@
+package domain;
+
+public class Result {
+
+    private int win;
+    private int drew;
+    private int lose;
+
+    public Result(int win, int drew, int lose) {
+        this.win = win;
+        this.drew = drew;
+        this.lose = lose;
+    }
+
+    public int getWin() {
+        return win;
+    }
+
+    public int getDrew() {
+        return drew;
+    }
+
+    public int getLose() {
+        return lose;
+    }
+
+    public void addWin() {
+        this.win++;
+    }
+}

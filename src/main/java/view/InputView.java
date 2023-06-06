805c5d4ba135f19bed9e6d4633ca4baaf3ae85c7
eom-tae-in <eti0728@daum.net>
2023-06-07 07:04:18 +0900

    [feat] 플레이어의 이름 입력을 받는 메서드 생성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,4 +5,13 @@ import java.util.Scanner;
 public class InputView {
 
     private Scanner sc = new Scanner(System.in);
+
+    public String[] getName() {
+        try {
+            String name = sc.nextLine();
+            return name.split(",");
+        } catch (RuntimeException e) {
+            return getName();
+        }
+    }
 }

923a02037ca2a1cdc98a4622cb34f5b759b23cad
eom-tae-in <eti0728@daum.net>
2023-06-06 11:07:55 +0900

    [test] 플레이어의 숫자의 합이 21이 넘는 경우를 검증하는 테스트 메서드 생성

--- src/test/java/domain/PlayerTest.java
+++ src/test/java/domain/PlayerTest.java
@@ -12,15 +12,38 @@ public class PlayerTest {
     @DisplayName("플레이어가 카드를 한장 더 받기를 원할 경우 카드 한장을 더 받는다.")
     void DrawMoreCard() {
         //given
+        CardDeck cardDeck = new CardDeck();
+        cardDeck.ready();
         UserDeck userDeck = new UserDeck();
         DrawCardDto drawCardDto = new DrawCardDto(new Card(new Shape("스페이드"), new Value(6)),
                 new Card(new Shape("하트"), new Value(4)));
-        Player player = new Player(new Name("apple"), userDeck);
+        Player player = new Player(new Name("apple"), userDeck, cardDeck);
         userDeck.add(drawCardDto);
+        String answer = "y";
+
         //when
-        player.drawCard();
+        player.drawCard(answer);
 
         //then
         assertThat(userDeck.getCards().size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("플레이어의 숫자의 합이 21이 넘는 경우 게임에서 지게 된다.")
+    void GameOver() {
+        //given
+        CardDeck cardDeck = new CardDeck();
+        cardDeck.ready();
+        UserDeck userDeck = new UserDeck();
+        userDeck.add(new DrawCardDto(new Card(new Shape("하트"), new Value(10))));
+        userDeck.add(new DrawCardDto(new Card(new Shape("스페이드"), new Value(10))));
+        userDeck.add(new DrawCardDto(new Card(new Shape("클로버"), new Value(10))));
+        Player player = new Player(new Name("apple"), userDeck, cardDeck);
+
+        //when
+        boolean result = player.isGameOver();
+
+        //then
+        assertThat(result).isTrue();
     }
 }

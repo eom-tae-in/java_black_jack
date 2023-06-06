1686585491a526af09cd2bbfe4e127ac1ce4253f
eom-tae-in <eti0728@daum.net>
2023-06-07 05:59:52 +0900

    [feat] 딜러와 플레이어의 공통되는 로직을 묶어서 사용하기 위해 Participant 클래스 생성

---
+++ src/main/java/domain/Participant.java
@@ -0,0 +1,30 @@
+package domain;
+
+public class Participant {
+
+    private final Name name;
+    private final UserDeck userDeck;
+    private final CardDeck cardDeck;
+
+    public Participant(Name name, UserDeck userDeck, CardDeck cardDeck) {
+        this.name = name;
+        this.userDeck = userDeck;
+        this.cardDeck = cardDeck;
+    }
+
+    public int getSum() {
+        return this.userDeck.sum();
+    }
+
+    public void getAdd(DrawCardDto drawCardDto) {
+        this.userDeck.add(drawCardDto);
+    }
+
+    public DrawCardDto getDraw() {
+        return this.cardDeck.draw();
+    }
+
+    public int getCalculateSum() {
+        return this.userDeck.calculateSum();
+    }
+}

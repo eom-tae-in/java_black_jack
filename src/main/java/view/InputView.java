3ffb15984a873d183e425c2b1f95997224a142b7
eom-tae-in <eti0728@daum.net>
2023-06-07 07:41:52 +0900

    [feat] 플레이어에게 카드를 더 받을 것인지 물어보는 메서드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,10 +1,13 @@
 package view;
 
+import exception.WrongInputException;
+
 import java.util.Scanner;
 
 public class InputView {
 
     private Scanner sc = new Scanner(System.in);
+    private final Validation validation = new Validation();
 
     public String[] getName() {
         try {
@@ -12,6 +15,16 @@ public class InputView {
             return name.split(",");
         } catch (RuntimeException e) {
             return getName();
+        }
+    }
+
+    public String getMore() {
+        try {
+            String answer = sc.next();
+            validation.checkGetMoreInput(answer);
+            return answer;
+        } catch (WrongInputException e) {
+            return getMore();
         }
     }
 }

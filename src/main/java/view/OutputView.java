48e02d283345d2bf10ecc13bb468d7485538da53
eom-tae-in <eti0728@daum.net>
2023-06-07 09:16:38 +0900

    [feat] 딜러의 카드들과 카드들의 합을 출력하는 메서드 생성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,6 @@
 package view;
 
+import domain.Dealer;
 import domain.Player;
 import domain.Players;
 
@@ -11,5 +12,11 @@ public class OutputView {
 
     public void printDealerDrew() {
         System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
+    }
+
+    private void printDealerDeck(Dealer dealer) {
+        String deck = dealer.getDeck().toString();
+        deck = deck.substring(1, deck.length() - 1);
+        System.out.println(dealer.getName() + " 카드: " + deck + " = 결과: "+ dealer.getSum());
     }
 }

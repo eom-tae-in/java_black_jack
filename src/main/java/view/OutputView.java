8a544d5dc33f8d7ff689dac56a65f315b0ea8e0b
eom-tae-in <eti0728@daum.net>
2023-06-07 09:55:47 +0900

    [feat] 이긴 경우가 없을 때는 빈칸 있는 경우 승을 출력하게 하는 메서드 생성

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -3,8 +3,11 @@ package view;
 import domain.Dealer;
 import domain.Player;
 import domain.Players;
+import domain.Result;
 
 public class OutputView {
+
+    private StringBuilder sb;
 
     public void AskMore(Player player) {
         System.out.println(player.getName() + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
@@ -31,5 +34,12 @@ public class OutputView {
         String deck = player.getDeck().toString();
         deck = deck.substring(1, deck.length() - 1);
         System.out.println(player.getName() + "카드: " + deck + " = 결과: " + player.getSum());
+    }
+
+    private String printWin(int winNumber) {
+        if (winNumber == 0) {
+            return " ";
+        }
+        return winNumber + "승 ";
     }
 }
